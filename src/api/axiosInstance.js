3d7205c2f66977d954be094b521ea090f62ca9bd
N VINOD KUMAR <nvinodkumar384@gmail.com>
2026-09-24 19:55:44 +0530

    Add request and response interceptors

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+let tokenStorage = null;
+export const getAuthToken = ()=>    tokenStorage;
+export const setAuthToken = (token)=>   tokenStorage=token;
+
+let logoutHandler = null;
+export const setLogoutHandler = (fun)=> logoutHandler=fun;
+
+
 //axios instance
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -7,5 +15,26 @@ const api = axios.create({
         'Content-Type': 'application/json',
     }
 })
+
+api.interceptors.request.use(
+    (config)=>{
+        if(tokenStorage)
+            config.headers.Authorization=`Bearer ${tokenStorage}`;
+
+        return config;
+    },
+     (error) =>   Promise.error(error)
+    )
+
+api.interceptors.response.use(
+    (response)=>response,
+    (error)=>{
+        if(error.response && error.response.status === 401){
+            //Invalid token so logout user
+            if(logoutHandler)
+                logoutHandler();
+        }
+    }
+);
 
 export default api;
